c64c607821b104f24d8bc49079e870428fe56f52
jardmclan <mcleanj@hawaii.edu>
2021-10-13 18:22:24 -1000

    add system id references, add system to cache

--- upload.py
+++ upload.py
@@ -1,6 +1,6 @@
 # Data uploader for workflow automation using Agave v2 for actors and file upload.
 
-import json, os
+import json
 from agavepy import Agave
 import pickle
 
@@ -15,7 +15,7 @@ global_cache_file: str = config["global_cache_file"]
 agave_options: dict = config["agave_options"]
 files_to_upload: list = config["upload"]
 
-folder_creation_cache = set()
+folder_creation_cache = {}
 if use_global_cache:
     try:
         with open(global_cache_file, "rb") as cache:
@@ -24,23 +24,28 @@ if use_global_cache:
         print("Could not find global cache. Creating a new one...")
 
 ag = Agave(**agave_options)
+#create token
+ag.token.create()
 
-# Finally, upload the files. Double-check this
-print(f"Files to upload: {files_to_upload}")
+# Finally, upload the files.
 for file_info in files_to_upload:
-
+    #relative to system root
     remote_path = file_info["remote_path"]
     local_path = file_info["local_path"]
+    system_id = file_info["system_id"]
 
-    if not remote_path in folder_creation_cache:
-        try:
-            #should recursively make parent directories as needed based on agave docs
-            ag.files.manageOnDefaultSystem(body = {"action": "mkdir", "path": remote_path}, sourceFilePath = "/")
-        #operation will fail if directory already exists (what exactly does it throw in this case? need to specify if adding retry, just pass for now)
-        except:
-            pass
+    system_cached_dirs = folder_creation_cache.get(system_id)
+    #system not cached, register with empty set
+    if system_cached_dirs is None:
+        system_cached_dirs = set()
+        folder_creation_cache[system_id] = system_cached_dirs
+
+    #if remote_path is not in system cache create and add to cache
+    if not remote_path in system_cached_dirs:
+        #will recursively create directory off of system root
+        ag.files.manage(body = {"action": "mkdir", "path": remote_path}, filePath = "", systemId = system_id)
         #add remote path to cache so not attempting to remake if multiple files use the same remote
-        folder_creation_cache.add(remote_path)
+        system_cached_dirs.add(remote_path)
 
     rename = file_info.get("rename")
 
@@ -48,12 +53,12 @@ for file_info in files_to_upload:
         #pack import arguments into dict so rename can be excluded if not specified
         importArgs = {
             "filePath": remote_path,
-            "fileToUpload": local_path
+            "fileToUpload": localFileToUpload,
+            "systemId": system_id,
         }
         if rename is not None:
-            importArgs["rename"] = rename
-        res = ag.files.importData(**importArgs)
-        print(res)
+            importArgs["fileName"] = rename
+        ag.files.importData(**importArgs)
 
 #dump cache to global if in use
 if use_global_cache:
