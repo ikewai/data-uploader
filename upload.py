9e02bec97d15c0a6c075a4bc94ee9acc819dbdeb
jardmclan <mcleanj@hawaii.edu>
2021-10-14 14:46:27 -1000

    add retry wrapper, add error handling and output, add exec stats

--- upload.py
+++ upload.py
@@ -3,6 +3,30 @@
 import json
 from agavepy import Agave
 import pickle
+import random
+import sys
+import time
+
+def get_backoff(delay):
+        backoff = 0
+        #if first failure backoff of 0.25-0.5 seconds
+        if delay == 0:
+            backoff = 0.25 + random.uniform(0, 0.25)
+        #otherwise 2-3x current backoff
+        else:
+            backoff = delay * 2 + random.uniform(0, delay)
+        return backoff
+
+def retry_wrapper(funct: function, args: dict, exceptions: tuple, retry: int, delay: float = 0):
+    try:
+        funct(**args)
+    except exceptions as e:
+        retry -= 1
+        if retry < 0:
+            raise e
+        backoff = get_backoff(delay)
+        retry_wrapper(funct, args, exceptions, retry, backoff)
+
 
 config = None
 
@@ -14,6 +38,8 @@ use_global_cache: bool = config["use_global_cache"]
 global_cache_file: str = config["global_cache_file"]
 agave_options: dict = config["agave_options"]
 files_to_upload: list = config["upload"]
+retry: int = config["retry"]
+print_exec_stats = config.get("print_exec_stats")
 
 folder_creation_cache = {}
 if use_global_cache:
@@ -26,6 +52,10 @@ if use_global_cache:
 ag = Agave(**agave_options)
 #create token
 ag.token.create()
+
+success = 0
+failed = 0
+start = time.time()
 
 # Finally, upload the files.
 for file_info in files_to_upload:
@@ -42,8 +72,19 @@ for file_info in files_to_upload:
 
     #if remote_path is not in system cache create and add to cache
     if not remote_path in system_cached_dirs:
-        #will recursively create directory off of system root
-        ag.files.manage(body = {"action": "mkdir", "path": remote_path}, filePath = "", systemId = system_id)
+        args = {
+            "body": {
+                "action": "mkdir",
+                "path": remote_path
+            },
+            "filePath": "",
+            "systemId": system_id
+        }
+        try:
+            #will recursively create directory off of system root
+            retry_wrapper(ag.files.manage, args, (Exception), retry)
+        except Exception as e:
+            print("Unable to create directory:\nsystem: %s\path: %s\nerror: %s" % (system_id, remote_path, repr(e)), file = sys.stderr)
         #add remote path to cache so not attempting to remake if multiple files use the same remote
         system_cached_dirs.add(remote_path)
 
@@ -51,16 +92,27 @@ for file_info in files_to_upload:
 
     with open(local_path, 'rb') as localFileToUpload:
         #pack import arguments into dict so rename can be excluded if not specified
-        importArgs = {
+        args = {
             "filePath": remote_path,
             "fileToUpload": localFileToUpload,
             "systemId": system_id,
         }
         if rename is not None:
-            importArgs["fileName"] = rename
-        ag.files.importData(**importArgs)
+            args["fileName"] = rename
+        try:
+            retry_wrapper(ag.files.importData, args, (Exception), retry)
+            success += 1
+        except Exception as e:
+            print("Unable to upload file:\nlocal: %s\nsystem: %s\nremote: %s\nerror: %s" % (local_path, system_id, remote_path, repr(e)), file = sys.stderr)
+            failed += 1
+
+if print_exec_stats:
+    end = time.time()
+    duration = end - start
+    print("File uploads complete:\nsuccess: %d, failed: %s, time: %.2f seconds")
 
 #dump cache to global if in use
 if use_global_cache:
     with open(global_cache_file, "wb") as cache:
         pickle.dump(folder_creation_cache, cache)
+
